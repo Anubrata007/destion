a6f33101faf497386c1ba0606771fbea25fa17fc
Anubrata007 <anubrata995@gmail.com>
2024-10-21 15:45:09 +0530

    add animatin in hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Black from '../assets/triangle.svg';
 import { TbMenu } from "react-icons/tb";
 import { IoIosSearch } from "react-icons/io";
+import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
 
@@ -47,59 +48,65 @@ const Navbar = () => {
 
 
     return (
-        <nav className="border-b sticky top-0 backdrop-blur-sm z-50">
-            <div className="flex justify-between item-center py-3 container mx-auto lg:px-0 px-4">
-                <div className="flex items-center">
-                    <div className="flex items-center gap-2">
-                        <a href="/">
-                            <img src={Black} alt='logo' className='h-12 w-12' />
-                        </a>
-                        <a href="/">
-                            <p className='text-3xl font-bold'>NEXT<spn className='text-xs font-'> .JS</spn></p>
-                        </a>
-                    </div>
-                    <div className="hidden lg:flex gap-6 ml-10 text-sm">
-                        {navItem.map((item) => (
-                            <a href={item.link} className="text-gray-500 hover:text-gray-950">
-                                {item.name}
+        <>
+            <nav className="border-b sticky top-0 backdrop-blur-sm z-50">
+                <div className="flex justify-between item-center py-3 container mx-auto lg:px-0 px-4">
+                    <div className="flex items-center">
+                        <div className="flex items-center gap-2">
+                            <a href="/">
+                                <img src={Black} alt='logo' className='h-12 w-12' />
                             </a>
-                        ))}
+                            <a href="/">
+                                <p className='text-3xl font-bold'>NEXT<spn className='text-xs font-'> .JS</spn></p>
+                            </a>
+                        </div>
+                        <div className="hidden lg:flex gap-6 ml-10 text-sm">
+                            {navItem.map((item) => (
+                                <a href={item.link} className="text-gray-500 hover:text-gray-950">
+                                    {item.name}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="lg:flex hidden items-center gap-4">
+                        <input
+                            type="text"
+                            className="w-full px-4 h-8 bg-gray-100 hover:bg-gray-200 rounded-md focus:outline-none text-sm text-gray-800"
+                            placeholder="Search documentation..."
+                        />
+
+                        <div className='flex'>
+                            <button
+                                type="submit"
+                                className="flex items-center justify-center w-28 h-8 border border-gray-300 rounded-md hover:bg-gray-100 text-sm text-center me-2 py-2 px-1 font-medium"
+                            >
+                                <img src={Black} alt='logo' className='h-8 w-8 mr-1' />
+                                Deploy
+                            </button>
+                            <button
+                                type="submit"
+                                className="bg-black text-white flex items-center justify-center h-8 rounded-md hover:bg-gray-800 text-sm text-center me-2 py-2 px-3"
+                            >
+                                Learn
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Hamburger and search icon */}
+                    <div className="lg:hidden flex items-center gap-3 pr-4">
+                        <button>
+                            <IoIosSearch className='h-6 w-6' />
+                        </button>
+                        <button onClick={toggleMenu} className={`transform transition-transform duration-300 ease-in-out ${isMenuOpen ? 'rotate-180' : 'rotate-0'}`}>
+                            {isMenuOpen ? (
+                                <RxCross1 className="h-6 w-6 transition-all duration-300" />
+                            ) : (
+                                <TbMenu className="h-6 w-6 transition-all duration-300" />
+                            )}
+                        </button>
                     </div>
                 </div>
-                <div className="lg:flex hidden items-center gap-4">
-                    <input
-                        type="text"
-                        className="w-full px-4 h-8 bg-gray-100 hover:bg-gray-200 rounded-md focus:outline-none text-sm text-gray-800"
-                        placeholder="Search documentation..."
-                    />
-
-                    <div className='flex'>
-                        <button
-                            type="submit"
-                            className="flex items-center justify-center w-28 h-8 border border-gray-300 rounded-md hover:bg-gray-100 text-sm text-center me-2 py-2 px-1 font-medium"
-                        >
-                            <img src={Black} alt='logo' className='h-8 w-8 mr-1' />
-                            Deploy
-                        </button>
-                        <button
-                            type="submit"
-                            className="bg-black text-white h-8 rounded-md hover:bg-gray-800 text-sm text-center me-2 py-2 px-3"
-                        >
-                            Learn
-                        </button>
-                    </div>
-                </div>
-
-                {/* Hamburger and search icon */}
-                <div className="lg:hidden flex items-center gap-3 pr-4">
-                    <button>
-                        <IoIosSearch className='h-6 w-6' />
-                    </button>
-                    <button onClick={toggleMenu}>
-                        <TbMenu className='h-6 w-6' />
-                    </button>
-                </div>
-            </div>
+            </nav>
 
             {/* Mobile menu */}
             {isMenuOpen && (
@@ -115,8 +122,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             )}
-
-        </nav>
+        </>
     )
 }
 
